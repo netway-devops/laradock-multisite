4815b7bd09c2d937d06d81e70ed4c78246aea913
Arnut Klamhui <arnut@netway.co.th>
2021-12-17 17:54:25 +0700

    update

--- local-ssl-certs-gen.sh
+++ local-ssl-certs-gen.sh
@@ -3,20 +3,19 @@
 
 mkcert -install
 
-declare -a arr=("cms.main"
+declare -a arr=(
+                "cms.main"
                 "cdn.rvtheme.com.main"
                 "license3.rvglobalsoft.com.main"
                 "netway.co.th.main"
-                "billing.netway.co.th.main"
                 "rvglobalsoft.com.main"
                 "client.netway.co.th.main"
+                "billing.netway.co.th.main"
                 )
 
 for i in "${arr[@]}"
 do
-   mkcert "$i"
-   mv "./${i}.pem" "./apache2/local-ssl-certs/"
-   mv "./${i}-key.pem" "./apache2/local-ssl-certs/"
+   mkcert -cert-file ./apache2/local-ssl-certs/${i}.pem -key-file ./apache2/local-ssl-certs/${i}-key.pem $i localhost 127.0.0.1 ::1
 done
 
 echo """
